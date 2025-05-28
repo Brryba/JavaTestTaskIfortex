21b8694cd39e38d415abc0a755d40683d4cf56fe
Yury Brazgunou <jurabrezgunov@gmail.com>
2025-05-28 22:27:02 +0300

    users queries added

--- src/main/java/com/example/java_ifortex_test_task/repository/UserRepository.java
+++ src/main/java/com/example/java_ifortex_test_task/repository/UserRepository.java
@@ -8,9 +8,21 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.List;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    @Query(value = "SELECT * FROM users", nativeQuery = true)
+    @Query(value = """
+            SELECT users.* FROM public.users
+            JOIN (SELECT max(started_at_utc) min_date, user_id
+               FROM public.sessions
+               WHERE sessions.device_type = :#{#deviceType.getCode()}
+               GROUP BY user_id) start_dates
+            ON users.id = start_dates.user_id
+            ORDER BY start_dates.min_date DESC""", nativeQuery = true)
     List<User> getUsersWithAtLeastOneMobileSession(DeviceType deviceType);
 
-    @Query(value = "SELECT * FROM users", nativeQuery = true)
+    @Query(value = """
+            SELECT users.* FROM public.sessions
+            JOIN public.users on sessions.user_id = users.id
+            GROUP BY users.id
+            ORDER BY count(sessions.id) DESC
+            LIMIT 1""", nativeQuery = true)
     User getUserWithMostSessions();
 }
